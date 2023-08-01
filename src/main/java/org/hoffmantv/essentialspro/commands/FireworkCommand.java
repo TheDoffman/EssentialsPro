99cea34009077804e0c9075114e865f88fe1aac2
Dan Hoffman <thedoffman@gmail.com>
2023-08-01 04:14:31 -0400

    push

--- src/main/java/org/hoffmantv/essentialspro/commands/FireworkCommand.java
+++ src/main/java/org/hoffmantv/essentialspro/commands/FireworkCommand.java
@@ -14,12 +14,14 @@ import org.bukkit.inventory.meta.FireworkMeta;
 
 import java.util.Random;
 
+// Handles the '/firework' command, which launches a random firework.
 public class FireworkCommand implements CommandExecutor {
+    private final Random random = new Random();
 
-    private Random random = new Random();
-
+    // When the command is executed
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure the command is being used by a player
         if (!(sender instanceof Player)) {
             Bukkit.getConsoleSender().sendMessage("This command can only be used by players!");
             return true;
@@ -27,25 +29,24 @@ public class FireworkCommand implements CommandExecutor {
 
         Player player = (Player) sender;
 
-        // Check for the appropriate permission
+        // Check if the player has the right permission
         if (!player.hasPermission("essentialspro.firework")) {
             player.sendMessage("You do not have permission to use this command!");
             return true;
         }
 
-        // Create a firework at the player's location
+        // Generate a firework at the player's location
         Firework firework = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
         FireworkMeta meta = firework.getFireworkMeta();
 
-        // Get a random firework type
-        Type[] types = Type.values();
-        Type type = types[random.nextInt(types.length)];
+        // Randomly decide the type of the firework
+        Type type = Type.values()[random.nextInt(Type.values().length)];
 
-        // Generate random colors for the firework
+        // Randomly generate main and fade colors
         Color color = getRandomColor();
         Color fade = getRandomColor();
 
-        // Define the firework effect
+        // Construct a firework effect with the randomly generated parameters
         FireworkEffect effect = FireworkEffect.builder()
                 .flicker(random.nextBoolean())
                 .trail(random.nextBoolean())
@@ -54,19 +55,19 @@ public class FireworkCommand implements CommandExecutor {
                 .withFade(fade)
                 .build();
 
-        // Apply the firework effect to the firework
+        // Set the firework's effect
         meta.addEffect(effect);
 
-        // Set the power level of the firework (this will affect how high it flies)
+        // Randomly set the power of the firework, affecting its flight duration
         meta.setPower(random.nextInt(3) + 1);
 
-        // Apply the meta to the firework
+        // Update the firework with the generated meta
         firework.setFireworkMeta(meta);
 
         return true;
     }
 
-    // Method to get a random color
+    // Helper method to generate a random color
     private Color getRandomColor() {
         return Color.fromBGR(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
